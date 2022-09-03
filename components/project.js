2d7330f7dd47c045f202c128a8ebe6fb3a200e57
Chris Chung <cchungdesign@gmail.com>
2022-09-03 02:42:56 -0700

    m^3 case study

--- components/project.js
+++ components/project.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import Image from 'next/image'
-import styles from './project.module.css'
-import Link from 'next/link';
+import React from "react";
+import Image from "next/image";
+import styles from "./project.module.css";
+import Link from "next/link";
 
 // function ProjectImage(props) {
 //     return (
@@ -10,29 +10,43 @@ import Link from 'next/link';
 // }
 
 export default function Project(props) {
-    // const project = [
-    //     {company: "Juniper Networks",
-    //     year: "2022",
-    //     name: "Mist AI Live View",
-    //     description: "Designing a solution for Juniper Network's Mist AI to set the rotation of access points automatically, saving time and increasing accuracy for location services administrators.",
-    //     role: "UI/UX Design, User Research, Copywriting, Prototyping"
-    // }
-    // ]
-    return (
-          <div className={styles.project}> 
-            <div className={styles.projectdesc}>
-              <div className={styles.company}>
-                <div>{props.company}</div>
-                <div>{props.year}</div>
-              </div>
-              <h2>{props.name}</h2>
-              
-              <p>{props.description}</p>
-              <p className={styles.subtext}><b>Role:</b> {props.role}</p>
-              
-              <p><a href="">Read more {'->'}</a></p>
-            </div>
-            <Image className={styles.img} src={props.img} alt={props.imgalt} width="400" height="300"/>
+  // const project = [
+  //     {company: "Juniper Networks",
+  //     year: "2022",
+  //     name: "Mist AI Live View",
+  //     description: "Designing a solution for Juniper Network's Mist AI to set the rotation of access points automatically, saving time and increasing accuracy for location services administrators.",
+  //     role: "UI/UX Design, User Research, Copywriting, Prototyping"
+  // }
+  // ]
+  return (
+    <div className={styles.project}>
+      <div className={styles.projecttext}>
+        <div className={styles.company}>
+          <div>
+            {props.company} · {props.year}
           </div>
-        )
-    }
+        </div>
+        <h2>{props.name}</h2>
+        <div className={styles.projectdesc}>
+          <p>{props.description}</p>
+          <p className={styles.subtext}>
+            <b>Role:</b> {props.role}
+          </p>
+        </div>
+
+        <p>
+          <a href={props.link}>Read more {"->"}</a>
+        </p>
+      </div>
+      <a href={props.link}>
+        <Image
+          className={styles.img}
+          src={props.img}
+          alt={props.imgalt}
+          width="600"
+          height="400"
+        />
+      </a>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../components/home.module.css'
-import Layout from '../components/layout.js';
-import Project from '../components/project.js'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../components/home.module.css";
+import Layout from "../components/layout.js";
+import Project from "../components/project.js";
+
+import m3 from "../public/ux/m3/2-mockup.png";
 
 export default function Home() {
   return (
@@ -13,38 +15,38 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Hi, I'm Chris.
-        </h1>
-
-        <p className={styles.description}>
-          I’m creating experiences for people. Pursuing Cognitive Science at <a href="https://www.pomona.edu/">Pomona College</a>. Currently designing at <a href="https://www.juniper.net/us/en.html">Juniper Networks</a>. ✨
-        </p>
+      <main className={styles.container}>
+        <div className={styles.description}>
+          <b>Chris Chung</b> is creating experiences for people. They are pursuing
+          Cognitive Science at{" "}
+          <a href="https://www.pomona.edu/">Pomona College</a>. Currently
+          designing at{" "}
+          <a href="https://www.juniper.net/us/en.html">Juniper Networks</a>.
+        </div>
 
         <div className={styles.projects}>
+          <Project
+            company="Juniper Networks"
+            year="2022"
+            name="Mist AI Live View"
+            description="Designing an automated solution to save time and increase accuracy for location services administrators."
+            role="UI/UX Design, User Research, Copywriting, Prototyping"
+            img="/../public/ux/juniper/1-main.jpeg"
+            imgalt="Juniper Networks Internship"
+          />
 
-        <Project 
-        company="Juniper Networks"
-        year="2022"
-        name="Mist AI Live View"
-        description="Designing a solution for Juniper Network's Mist AI to set the rotation of access points automatically, saving time and increasing accuracy for location services administrators."
-        role="UI/UX Design, User Research, Copywriting, Prototyping"
-        img="/../public/ux/juniper/1-main.jpeg"
-        imgalt="Juniper Networks Internship"
-        />
-
-        <Project 
-        company="M^3"
-        year="2021"
-        name="Virtual Try-On"
-        description="Designing a mask sizer to help provide custom-fit masks to underserved and marginalized communities."
-        role="UI/UX Design, Brand Identity, Creative Direction, AI"
-        img="/../public/ux/m3/1-main.png"
-        imgalt="M^3 Internship"
-        />
+          <Project
+            company="M^3"
+            year="2021"
+            name="Virtual Try-On"
+            description="Designing a mask sizer to help provide custom-fit masks to underserved and marginalized communities."
+            role="UI/UX Design, Brand Identity, Creative Direction, AI"
+            img={m3}
+            imgalt="M^3 Internship"
+            link="/ux-m3"
+          />
         </div>
       </main>
     </Layout>
-  )
+  );
 }
