422da49e2f3da3204564bcd296301a9e09ba2cd2
Chris Chung <cchungdesign@gmail.com>
2022-09-07 11:46:14 -0700

    added colors mag

--- pages/digital-media.js
+++ pages/digital-media.js
@@ -20,6 +20,15 @@ import warmth_2 from "/public/digital-media/warmth/render.png";
 import warmth_3 from "/public/digital-media/warmth/design.png";
 const warmth = [warmth_1, warmth_2, warmth_3];
 
+import colors_mag_1 from "/public/digital-media/colors-mag/1.jpeg";
+import colors_mag_2 from "/public/digital-media/colors-mag/2.jpeg";
+import colors_mag_3 from "/public/digital-media/colors-mag/3.jpeg";
+const colors_mag = [
+  colors_mag_1,
+  colors_mag_2,
+  colors_mag_3,
+];
+
 // works
 // const SLIDE_COUNT = 3;
 // const slides = Array.from(Array(SLIDE_COUNT).keys());
@@ -43,6 +52,15 @@ export default function DigitalMedia() {
           year="2020"
           description="warmth is a conceptual tea house specializing in organic Taiwanese tea. The brand provides a friendly, welcoming atmosphere through warm colors and elegant design. The special osmanthus and oolong tea blend is represented by an abstract illustration with soft gradients and figure dualism. The sweet orange and indigo colors are chosen to represent the growing osmanthus plants and create a warm visual identity."
           images={warmth}
+        />
+      </div>
+      <div className={styles.container}>
+        <GalleryItem
+          title="COLORS MAG"
+          id="colors-mag"
+          year="2019"
+          description="COLORS MAG is a project about developing personal style, influence of colors, and individuality. Each release of the lookbook showcases an individual through a prevailing color, photography of their outfit, and an interview about their interest in fashion."
+          images={colors_mag}
         />
       </div>
     </Layout>
