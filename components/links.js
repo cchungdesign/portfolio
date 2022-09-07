49488200d224fc67da8e54e8d1af8977612e8c51
Chris Chung <cchungdesign@gmail.com>
2022-09-07 12:50:34 -0700

    added temporary tab links

--- components/links.js
+++ components/links.js
@@ -7,6 +7,8 @@ export default function Links() {
   <div className={styles.container}>
     <Link href="/#work"><a>&rarr; Work</a></Link>
     <Link href="/digital-media"><a>&#8594; Digital Media</a></Link>
+    <a href="https://cchung.myportfolio.com/photography" target="_blank">&#8594; Photography</a>
+    <a href="https://cchung.myportfolio.com/illustration" target="_blank">&#8594; Illustration</a>
     {/* <Link href="/photography"><a>&#8594; Photography</a></Link>
     <Link href="/illustration"><a>&#8594; Illustration</a></Link> */}
   </div>
