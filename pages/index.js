0bad35f290d9494cef74dcb3f687220cd493936c
Chris Chung <cchungdesign@gmail.com>
2024-01-17 13:53:41 -0800

    gradient flower

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Links from "/components/links.js";
 import mcubed from "/public/work/mcubed/2-mockup.png";
 import juniper from "/public/work/juniper/cover.png";
 import harpie from "/public/work/harpie/coverblue.png";
-import bg from "/public/flower-bg.png";
+import bg from "/public/flower-bg-gradient.png";
 
 export default function Home() {
   return (
