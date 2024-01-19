7a85dc6a1580a52d2e5ad015e396423cc1e1dc7c
Chris Chung <cchungdesign@gmail.com>
2024-01-19 11:33:31 -0800

    fixed image css

--- components/imgcaption.js
+++ components/imgcaption.js
@@ -5,8 +5,10 @@ import styles from "./projectpage.module.css";
 export default function ImgCaption(props) {
   return (
     <div>
-      <Image src={props.img} />
-      <div className={styles.caption}>{props.caption}</div>
+      <div className={styles.imgcaption}>
+        <Image src={props.img} />
+        <div className={styles.caption}>{props.caption}</div>
+      </div>
     </div>
   );
 }
