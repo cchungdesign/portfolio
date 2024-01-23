5253c11253598841eb1c026cd59caadeaf132cea
Chris Chung <cchungdesign@gmail.com>
2024-01-23 11:41:27 -0800

    changed label names

--- components/links.js
+++ components/links.js
@@ -1,15 +1,30 @@
-import React from 'react'
-import styles from './links.module.css'
-import Link from 'next/link';
+import React from "react";
+import styles from "./links.module.css";
+import Link from "next/link";
 
 export default function Links() {
   return (
-  <div className={styles.container}>
-    <span>&#8594; <Link href="/#work"><a>Work</a></Link></span>
-    <span>&#8594; <Link href="/digital-media"><a>Digital Media</a></Link></span>
-    {/* <span>&#8594; <Link href="https://cchung.myportfolio.com/photography"><a>Photography</a></Link></span>
+    <div className={styles.container}>
+      <span>
+        &#8594;{" "}
+        <Link href="/#work">
+          <a>Work</a>
+        </Link>
+      </span>
+      <span>
+        &#8594;{" "}
+        <Link href="/digital-media">
+          <a>Digital Media</a>
+        </Link>
+      </span>
+      {/* <span>&#8594; <Link href="https://cchung.myportfolio.com/photography"><a>Photography</a></Link></span>
     <span>&#8594; <Link href="https://cchung.myportfolio.com/illustration"><a>Illustration</a></Link></span> */}
-    <span>&#8594; <Link href="https://chrischung.net/"><a>Personal Site</a></Link></span>
-  </div>
-  )
+      <span>
+        &#8594;{" "}
+        <Link href="https://chrischung.net/">
+          <a>Art (chrischung.net)</a>
+        </Link>
+      </span>
+    </div>
+  );
 }
